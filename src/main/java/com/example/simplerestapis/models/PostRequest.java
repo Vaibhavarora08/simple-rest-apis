e0624239c4ec178fc6a9facdcf4346c7c8750187
Vaibhavarora08 <38854840+Vaibhavarora08@users.noreply.github.com>
2019-06-11 08:56:33 +0530

    Update PostRequest.java

--- src/main/java/com/example/simplerestapis/models/PostRequest.java
+++ src/main/java/com/example/simplerestapis/models/PostRequest.java
@@ -1,19 +1,34 @@
 package com.example.simplerestapis.models;
 
 public class PostRequest {
-	int id;
-	String name;
-	public int getId() {
-		return id;
+	long version;
+        String lastcommitsha;
+	String description;
+
+	public long getversion() {
+		return version;
 	}
-	public String getName() {
-		return name;
+
+	public void setversion(long version) {
+		this.version = version;
 	}
-	public void setId(int id) {
-		this.id = id;
+
+	public String getlastcommitsha() {
+		return lastcommitsha;
 	}
-	public void setName(String name) {
-		this.name = name;
+
+	public void setlastcommiitsha(long lastcommitsha) {
+		this.lastcommitsha = lastcommitsha;
+	}
+	
+	public String getdescription() {
+		return description;
+	}
+
+	public void setdescription(String description) {
+		this.description = description;
 	}
 
 }
+
+
